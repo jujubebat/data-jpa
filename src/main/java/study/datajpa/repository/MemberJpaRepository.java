a12c695603c06ebc37ed3987eac0d4ee9f50a460
bbwwpark <bbwwpark@naver.com>
2022-01-23 22:16:39 +0900

    feat: 순수 jpa 페이징, 정렬 학습

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -56,4 +56,17 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
+    public List<Member> findByPage(int age, int offset, int limit) { // 페이징 쿼리
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset) // 어디서부터
+                .setMaxResults(limit) // 몇개를 가져올건지
+                .getResultList();
+    }
+
+    public long totalCount(int age) { // 토탈 개수 쿼리
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
